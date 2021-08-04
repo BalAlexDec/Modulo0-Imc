3e85a1c373db618b36b65b5a10d9bb7eac5b4980
Alex Carrasco Balart <44027899+BalAlexDec@users.noreply.github.com>
2021-08-04 18:40:44 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+function imc(){
+  var height = prompt("Introdueix la teva altura, Sense . ","");
+
+  var weight = prompt("Introdueix el teu pes","");
+  height = height/100;
+  console.log(height);
+  
+  console.log(weight);
+ 
+
+  if(height!=null&&weight!=null){
+    
+  var imc = weight/(height*height);
+  console.log(imc.toFixed(3));
+    if(imc<18.5){ alert(imc.toFixed(3) +" "+"Peso insuficiente"); }
+			else if(imc>=18.5 && imc<=24.9){ alert(imc.toFixed(3) + " "+ "Peso normal");  }
+			else if(imc>=25 && imc<=26.9){ alert(imc.toFixed(3) +" "+"Sobrepeso grado 1");  }
+      else if(imc>=27 && imc<=29.9){ alert(imc.toFixed(3) +" "+"Sobrepeso grado 2");  }
+      else if(imc>=30 && imc<=34.9){ alert(imc.toFixed(3) +" "+"Obesidad grado 1");  }
+      else if(imc>=35 && imc<=39.9){ alert(imc.toFixed(3) +" "+"Obesidad grado 2");  }
+      else if(imc>=40 && imc<=49.9){ alert(imc.toFixed(3) +" "+"Obesidad Morbida");  }
+			else if(imc>49.9){ alert(imc.toFixed(3) +" "+"Obesidad Extrema"); }
+
+    }
+}
+
+
